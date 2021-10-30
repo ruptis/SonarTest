80ad5be73e9339618da70b217262fadbdcad1517
ruptis <90418759+ruptis@users.noreply.github.com>
2021-10-30 20:28:05 +0300

    test

---
+++ app/src/main/java/com/example/stringexpressioncalculator/Calculator.java
@@ -0,0 +1,193 @@
+package com.example.stringexpressioncalculator;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Calculator implements ICalculator {
+    private static final String INVALID_INPUT = "Invalid input";
+    private static final String DIV_BY_ZERO = "Divided by zero";
+
+    public double calculate(String expression) throws CalculatorException {
+        expression = expression
+                .replaceFirst("^-", "0-")
+                .replace("(-", "(0-");
+
+        double result;
+
+        try {
+            result = getAnswer(getReversePolishNotation(getItems(expression)));
+        } catch (CalculatorException e) {
+            throw new CalculatorException(e.getMessage());
+        } catch (Exception e) {
+            throw new CalculatorException(INVALID_INPUT);
+        }
+
+        return result;
+    }
+
+    private ArrayList<String> getItems(String expression) {
+        ArrayList<String> items = new ArrayList<>();
+
+        String regex = "([0-9]+[.]$)|([0-9]*[.]?[0-9]+)|([0-9]+[.]?[0-9]*)|[+\\-*^/()]";
+
+        Pattern pattern = Pattern.compile(regex);
+
+        Matcher matcher = pattern.matcher(expression);
+
+        while (matcher.find()){
+            items.add(matcher.group());
+        }
+
+        return items;
+    }
+
+    private ArrayList<String> getReversePolishNotation(ArrayList<String> items) {
+        Deque<String> stack = new ArrayDeque<>();
+
+        ArrayList<String> itemsRPN = new ArrayList<>();
+
+        for (String item : items) {
+            int itemPrior = getPriority(item);
+
+            switch (itemPrior) {
+                case 0:
+                    itemsRPN.add(item);
+                    break;
+                case 4:
+                    stack.push(item);
+                    break;
+                case 5:
+                    while (getPriority(Objects.requireNonNull(stack.peek())) != 4) {
+                        itemsRPN.add(stack.pop());
+                    }
+
+                    stack.pop();
+                    break;
+                default:
+                    if (!stack.isEmpty()) {
+                        boolean isPop;
+
+                        do {
+                            isPop = false;
+
+                            int latestItemPrior = getPriority(Objects.requireNonNull(stack.peek()));
+
+                            if (latestItemPrior >= itemPrior && latestItemPrior < 4) {
+                                itemsRPN.add(stack.pop());
+
+                                isPop = true;
+                            }
+                        } while (isPop && !stack.isEmpty());
+
+                    }
+
+                    stack.push(item);
+                    break;
+            }
+        }
+
+        if (!stack.isEmpty()) {
+            do {
+                itemsRPN.add(stack.pop());
+            } while (!stack.isEmpty());
+        }
+
+        return itemsRPN;
+    }
+
+    private double getAnswer(ArrayList<String> itemsRPN) throws CalculatorException {
+        if (itemsRPN.contains("(")) {
+            throw new CalculatorException(INVALID_INPUT);
+        }
+
+        double temp = 0;
+
+        int i = 0;
+
+        while (i < itemsRPN.size()) {
+            boolean isOperator = true;
+
+            switch (itemsRPN.get(i)) {
+                case "+": {
+                    temp = Double.parseDouble(itemsRPN.get(i - 2)) + Double.parseDouble(itemsRPN.get(i - 1));
+                    break;
+                }
+                case "-": {
+                    temp = Double.parseDouble(itemsRPN.get(i - 2)) - Double.parseDouble(itemsRPN.get(i - 1));
+                    break;
+                }
+                case "*": {
+                    temp = Double.parseDouble(itemsRPN.get(i - 2)) * Double.parseDouble(itemsRPN.get(i - 1));
+                    break;
+                }
+                case "/": {
+                    if (Double.parseDouble(itemsRPN.get(i - 1)) == 0) {
+                        throw new CalculatorException(DIV_BY_ZERO);
+                    }
+
+                    temp = Double.parseDouble(itemsRPN.get(i - 2)) / Double.parseDouble(itemsRPN.get(i - 1));
+                    break;
+                }
+                case "^": {
+                    temp = Math.pow(Double.parseDouble(itemsRPN.get(i - 2)), Double.parseDouble(itemsRPN.get(i - 1)));
+                    break;
+                }
+                default: isOperator = false;
+            }
+
+            if (isOperator) {
+                itemsRPN.remove(i - 2);
+                itemsRPN.remove(i - 2);
+                itemsRPN.set(i - 2, "" + temp);
+
+                i = 0;
+            } else {
+                i++;
+            }
+        }
+
+        if (itemsRPN.size() > 1) {
+            throw new CalculatorException(INVALID_INPUT);
+        }
+
+        return Double.parseDouble(itemsRPN.get(0));
+    }
+
+    private static int getPriority(String item) {
+        int priority = 0;
+
+        switch (item) {
+            case ")": {
+                priority = 5;
+                break;
+            }
+            case "(": {
+                priority = 4;
+                break;
+            }
+            case "^": {
+                priority = 3;
+                break;
+            }
+            case "*":
+            case "/": {
+                priority = 2;
+                break;
+            }
+            case "+":
+            case "-": {
+                priority = 1;
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+
+        return priority;
+    }
+}
---
+++ app/src/main/java/com/example/stringexpressioncalculator/MainActivity.java
@@ -0,0 +1,134 @@
+package com.example.stringexpressioncalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.text.Html;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.HorizontalScrollView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    ArrayList<String> inputItems = new ArrayList<>();
+
+    public static final int SPACE = 15;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        onWindowFocusChanged(true);
+
+        if (Build.VERSION.SDK_INT >= 28) {
+            getWindow().getAttributes().layoutInDisplayCutoutMode =
+                    WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+        }
+    }
+
+    @SuppressLint({"NonConstantResourceId", "SetTextI18n"})
+    public void onClickStart(View view) {
+        int id = view.getId();
+        final Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        final VibrationEffect vibrationEffect;
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+
+            vibrationEffect = VibrationEffect.createPredefined(VibrationEffect.EFFECT_CLICK);
+
+            vibrator.cancel();
+
+            vibrator.vibrate(vibrationEffect);
+        }
+
+        IExpressionChecker expressionChecker = new ExpressionChecker();
+
+        TextView input = findViewById(R.id.input);
+        TextView output = findViewById(R.id.output);
+        HorizontalScrollView scroll = (HorizontalScrollView) findViewById(R.id.scroll_inp);
+
+        output.setTextSize(TypedValue.COMPLEX_UNIT_PX, getResources().getDimension(R.dimen.baseText));
+
+        switch (id) {
+            case R.id.deleteAll: {
+                inputItems.clear();
+                output.setText("");
+                break;
+            }
+            case R.id.delete: {
+                if (!inputItems.isEmpty()) {
+                    inputItems.remove(inputItems.size() - 1);
+                }
+                break;
+            }
+            case R.id.solve: {
+                output.setTextSize(TypedValue.COMPLEX_UNIT_PX, getResources().getDimension(R.dimen.largeText));
+                break;
+            }
+            default: {
+                String currentItem = ((TextView) findViewById(id)).getText().toString();
+
+                if (expressionChecker.check(inputItems, currentItem)) {
+                    inputItems.add(currentItem);
+                }
+            }
+        }
+
+        StringBuilder items = new StringBuilder();
+
+        for (String inputItem : inputItems) {
+            items.append(inputItem);
+        }
+
+        input.setText(items.toString());
+
+        scroll.post(new Runnable() {
+            @Override
+            public void run() {
+                scroll.fullScroll(View.FOCUS_RIGHT);
+            }
+        });
+
+        ICalculator calculator = new Calculator();
+
+        AnswerFormat answerFormat = new AnswerFormat();
+
+        String outputResult;
+
+        if (items.length() > 0) {
+            try {
+                double result = calculator.calculate(items.toString()
+                        .replace("e", "" + Math.E)
+                        .replace(Html.fromHtml("&#960;"), "" + Math.PI)
+                        .replace(",", "."));
+
+                if (Double.isInfinite(result)) {
+                    outputResult = "Infinity";
+                } else if (Double.isNaN(result)) {
+                    outputResult = "NaN";
+                } else {
+                    outputResult = answerFormat.getFormattedAnswer(result, SPACE);
+                }
+            } catch (CalculatorException e) {
+                outputResult = e.getMessage();
+            }
+        } else {
+            outputResult = "";
+        }
+
+        output.setText(outputResult);
+
+    }
+}
